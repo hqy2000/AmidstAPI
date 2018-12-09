644495ae02ba5f026ecca08e7100eb03f17cd50a
Qingyang Hu <huqingyang2000@gmail.com>
2018-12-09 18:31:57 +0800

    Add command line

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Amidst API
+A command line tool to help you extract biome data from Minecraft. 
+This tool uses [amidst](https://github.com/toolbox4minecraft/amidst/releases) to communicate with official Minecraft jar file and extract information.
+
+## Usage
+You need Java Runtime Environment to run this. We encourage you to use Java 8.
+
+Usage:
+``` java -jar amidst_api.jar dot_folder version seed radius```
+
+E.g. ```java -jar AmidstAPI.jar "C:\\Users\\huqin\\AppData\\Roaming\\.minecraft" "1.13.2" -6271427238188031176 1```
+
+```dot_folder``` is where your ```.minecraft``` folder is located, e.g. ```C:\Users\huqin\AppData\Roaming\.minecraft```. Please remember to escape '\\'.
+
+```version``` is the Minecraft version that you want to use to generate the biome, e.g. ```1.13.2```
+
+```seed``` is the seed that you use for generating the world, e.g. ```-6271427238188031176```
+
+```radius``` The area generated is the **square (not circle!)** with the coordinates ```((radius*1024, radius*1024), (-radius*1024, -radius*1024))```. ```10``` is enough for most of the situations.
+
+Data are exported to ```out``` folder. It usually takes 1 hour when the radius is ```10``` (i7-7820HQ, SSD).
+
+## Development
+Download amidst jar file from the its [releases](https://github.com/toolbox4minecraft/amidst/releases), and add it as a library. 
--- src/com/ilufang/Main.java
+++ src/com/ilufang/Main.java
@@ -6,46 +6,35 @@ import amidst.mojangapi.file.LauncherProfile;
 import amidst.mojangapi.world.WorldType;
 import amidst.mojangapi.file.MinecraftInstallation;
 import amidst.mojangapi.file.directory.DotMinecraftDirectory;
-
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.List;
 
 public class Main {
-    private static final int ARG_MCJARFILE = 1;
+    private static final int ARG_DOT_FOLDER = 0;
+    private static final int ARG_VERSION = 1;
     private static final int ARG_SEED = 2;
-    private static final int ARG_CHUNKX = 3;
-    private static final int ARG_CHUNKY = 4;
+    private static final int ARG_RADIUS = 3;
 
     private static MinecraftInterface mc;
 
     public static void main(String[] args) {
-        if (args.length<5) {
-            System.err.println("Invalid Args. Usage: [jarFile] [seed] [chunkX] [chunkY]");
-            //return;
+        if (args.length<4) {
+            System.err.println("Invalid Args. Usage: java -jar amidst_api.jar dot_folder version seed radius");
+            return;
         }
 
-        File lockFile = new File("AMIDST_LOCK");
-        if (lockFile.exists()) {
-            System.err.println("Concurrent modification.");
-            //return;
-        }
-
-        //File mcJar = new File(args[ARG_MCJARFILE]);
-        File mcPath = new File("C:\\Users\\huqin\\AppData\\Roaming\\.minecraft");
+        File mcPath = new File(args[ARG_DOT_FOLDER]);
         try {
-            lockFile.createNewFile();
             DotMinecraftDirectory directory = new DotMinecraftDirectory(mcPath);
             MinecraftInstallation install = new MinecraftInstallation(directory);
             List<LauncherProfile> profileList = install.readInstalledVersionsAsLauncherProfiles();
             profileList.forEach((v)->{
-                if (v.getVersionName().equals("*1.13.2")) {
+                if (v.getVersionName().equals("*" + args[ARG_VERSION])) {
                     try {
                         mc = MinecraftInterfaces.fromLocalProfile(v);
                     } catch (Exception e) {
                         System.err.println("Failed to load Minecraft jar.");
-                        lockFile.delete();
                     }
 
                 }
@@ -53,16 +42,13 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Failed to load Minecraft jar.");
-            lockFile.delete();
             return;
         }
-        //mc.createWorld(Long.parseLong(args[ARG_SEED]), "default", "");
         try {
-            mc.createWorld(-6271427238188031176L, WorldType.DEFAULT, "");
+            mc.createWorld(Long.parseLong(args[ARG_SEED]), WorldType.DEFAULT, "");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Failed to create world.");
-            lockFile.delete();
             return;
         }
 
@@ -77,35 +63,33 @@ public class Main {
         // Part 3: 1 Byte for Number of villages, followed by sets of 3 UInt8 for Relative Chunk Coordinates (2byte) and Coordinate in Chunk (1byte)
         // Part 4-8: Similar calculation for Witch Hut, Desert Temple, Jungle Temple, Stronghold, and Ocean Monument
 
-        //int beginX = Integer.parseInt(args[ARG_CHUNKX]);
-        //int beginY = Integer.parseInt(args[ARG_CHUNKY]);
-        int beginX = 0, beginY = 0;
-        for (int x=-10; x<=10; x++) for (int y=-10; y<=10; y++) {
+        int radius = Integer.parseInt(args[ARG_RADIUS]);
+        File outputDir = new File("out");
+        if (!outputDir.exists()) {
+            outputDir.mkdir();
+        }
+        for (int x=-radius; x<=radius; x++) for (int y=-radius; y<=radius; y++) {
             try {
                 System.out.println("Generating "+x+","+y);
-                File biomeFile = new File("biomes" + x + "_" + y);
+                File biomeFile = new File("out/biomes" + x + "_" + y);
                 if (!biomeFile.exists()) {
                     biomeFile.createNewFile();
                 }
                 FileOutputStream os = new FileOutputStream(biomeFile);
-                //DataOutputStream dataos = new DataOutputStream(os);
                 // 1. Biomes
                 int[] biomes = mc.getBiomeData(x*1024, y*1024, 1024, 1024, true);
                 byte[] bytebiomes = new byte[biomes.length];
                 for (int i=0; i<biomes.length; i++) {
                     bytebiomes[i] = (byte)biomes[i];
-                    //dataos.writeByte(biomes[i]);
                 }
                 os.write(bytebiomes);
 
             } catch (Exception e) {
                 e.printStackTrace();
                 System.err.println("File operation error.");
-                lockFile.delete();
                 return;
             }
         }
-        lockFile.delete();
         System.out.println("Completed.");
     }
 }
